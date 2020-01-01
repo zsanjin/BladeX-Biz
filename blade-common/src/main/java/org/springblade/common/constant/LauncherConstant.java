deb22cfdb05000ecc7c6eff19626a48ff35bf822
Crystal_zhi <924518270@qq.com>
2020-01-01 16:09:48 +0800

    update local nacos address

--- blade-common/src/main/java/org/springblade/common/constant/LauncherConstant.java
+++ blade-common/src/main/java/org/springblade/common/constant/LauncherConstant.java
@@ -35,7 +35,7 @@ public interface LauncherConstant {
 	/**
 	 * nacos dev 地址
 	 */
-	String NACOS_DEV_ADDR = "127.0.0.1:8848";
+	String NACOS_DEV_ADDR = "192.168.101.21:8848";
 
 	/**
 	 * nacos prod 地址
